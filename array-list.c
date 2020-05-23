883bd0d31ebe12b0d1de72438bdbc74a22902b25
OxyOCE <harry.smith.0119@gmail.com>
2020-05-23 18:14:59 +1200

    Implemented append and get for linked list

--- array-list.c
+++ array-list.c
@@ -13,6 +13,26 @@ array_list array_list_new()
     return ret;
 }
 
+void array_list_append(array_list al, int elem)
+{
+    if (al->size >= al->capacity) {
+        al->capacity *= 2;
+        al->array = erealloc(al->array, al->capacity * sizeof al->array[0]);
+    }
+
+    al->array[al->size++] = elem;
+}
+
+int array_list_get(array_list al, int *dest, long idx)
+{
+    if (idx >= al->size || idx < 0) {
+        return -1;
+    }
+
+    *dest = al->array[idx];
+    return 0;
+}
+
 void array_list_free(array_list al)
 {
     free(al->array);
--- include/array-list.h
+++ include/array-list.h
@@ -9,6 +9,8 @@ typedef struct array_list
 } *array_list;
 
 extern array_list array_list_new();
+extern void       array_list_append(array_list al, int elem);
+extern int        array_list_get(array_list al, int *dest, long idx);
 extern void       array_list_free(array_list al);
 
 #endif
