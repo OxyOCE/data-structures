d3aa1cd677394c8b64a3f5eef69b25318407b061
OxyOCE <harry.smith.0119@gmail.com>
2020-05-28 17:40:44 +1200

    Implemented heapsort

---
+++ src/heapsort.c
@@ -0,0 +1,52 @@
+#include "include/heapsort.h"
+#include "include/common.h"
+#include "include/defs.h"
+
+static void sift_down(int *array, long start, long end)
+{
+    int root, l_child, swap_idx;
+
+    root = start;
+    while (2 * root + 1 <= end) {
+        l_child = 2 * root + 1;
+        swap_idx = root;
+
+        if (array[swap_idx] < array[l_child]) {
+            swap_idx = l_child;
+        }
+        if (l_child + 1 <= end && array[swap_idx] < array[l_child + 1]) {
+            swap_idx = l_child + 1;
+        }
+        if (swap_idx == root) {
+            return;
+        } else {
+            swap(&array[root], &array[swap_idx]);
+            root = swap_idx;
+        }
+    }
+}
+
+static void heapify(int *array, long size)
+{
+    int start;
+
+    start = (size - 2) / 2;
+    while (start >= 0) {
+        sift_down(array, start--, size - 1);
+    }
+}
+
+int heapsort(int *array, long size)
+{
+    int end;
+
+    heapify(array, size);
+
+    end = size - 1;
+    while (end > 0) {
+        swap(&array[end--], &array[0]);
+        sift_down(array, 0, end);
+    }
+
+    return SUCCESS;
+}
--- test/sorting-tests.c
+++ test/sorting-tests.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include "include/heapsort.h"
 #include "include/mergesort.h"
 #include "include/quicksort.h"
 #include "include/simple-algorithms.h"
@@ -50,22 +51,25 @@ static int compare(const void *a, const void *b)
 static void run_suite(int *suite[], int suite_size, int suite_sizes[])
 {
     int i, j, test_size;
-    int *scratch_array_mergesort, *scratch_array_insertion_sort, *scratch_array_quicksort, *scratch_array_qsort;
+    int *scratch_array_heapsort, *scratch_array_mergesort, *scratch_array_insertion_sort, *scratch_array_quicksort, *scratch_array_qsort;
 
     for (i = 0; i < suite_size; i++) {
         test_size = suite_sizes[i];
+        scratch_array_heapsort = emalloc(test_size * sizeof scratch_array_heapsort[0]);
         scratch_array_mergesort = emalloc(test_size * sizeof scratch_array_mergesort[0]);
         scratch_array_insertion_sort = emalloc(test_size * sizeof scratch_array_insertion_sort[0]);
         scratch_array_quicksort = emalloc(test_size * sizeof scratch_array_quicksort[0]);
         scratch_array_qsort = emalloc(test_size * sizeof scratch_array_qsort[0]);
 
         for (j = 0; j < test_size; j++) {
+            scratch_array_heapsort[j] = suite[i][j];
             scratch_array_mergesort[j] = suite[i][j];
             scratch_array_insertion_sort[j] = suite[i][j];
             scratch_array_quicksort[j] = suite[i][j];
             scratch_array_qsort[j] = suite[i][j];
         }
 
+        heapsort(scratch_array_heapsort, test_size);
         msort(scratch_array_mergesort, test_size, 0, test_size - 1);
         insertion_sort(scratch_array_insertion_sort, test_size);
         quicksort(scratch_array_quicksort, test_size, 0, test_size - 1);
@@ -73,11 +77,13 @@ static void run_suite(int *suite[], int suite_size, int suite_sizes[])
 
         for (j = 0; j < test_size; j++)
         {
+            assert(scratch_array_heapsort[j] == scratch_array_qsort[j]);
             assert(scratch_array_mergesort[j] == scratch_array_qsort[j]);
             assert(scratch_array_insertion_sort[j] == scratch_array_qsort[j]);
             assert(scratch_array_quicksort[j] == scratch_array_qsort[j]);
         }
 
+        free(scratch_array_heapsort);
         free(scratch_array_mergesort);
         free(scratch_array_insertion_sort);
         free(scratch_array_quicksort);
