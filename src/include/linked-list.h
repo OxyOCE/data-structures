06ef1fb42a8a23291303bfdb2012f663fc3daee9
OxyOCE <harry.smith.0119@gmail.com>
2020-05-28 23:16:41 +1200

    Added linked list skeleton code

---
+++ src/include/linked-list.h
@@ -0,0 +1,30 @@
+#ifndef LINKED_LIST_H_
+#define LINKED_LIST_H_
+
+typedef struct linked_list_node
+{
+    int data;
+    struct linked_list_node *prev;
+    struct linked_list_node *next;
+} *linked_list_node;
+
+typedef struct linked_list
+{
+    linked_list_node head;
+    linked_list_node tail;
+    long size;
+} *linked_list;
+
+extern linked_list linked_list_new();
+extern void        linked_list_append(linked_list ll, int elem);
+extern int         linked_list_insert(linked_list ll, int elem, long idx);
+extern int         linked_list_set(linked_list ll, int elem, long idx);
+extern int         linked_list_delete(linked_list ll, int *dest, long idx);
+extern int         linked_list_get(linked_list ll, int *dest, long idx);
+extern int         linked_list_find(linked_list ll, int elem, long start, int mode);
+extern int         linked_list_sort(linked_list ll);
+extern void        linked_list_reverse(linked_list ll);
+extern void        linked_list_print(linked_list ll);
+extern void        linked_list_free(linked_list ll);
+
+#endif
---
+++ src/linked-list.c
@@ -0,0 +1,56 @@
+#include "include/linked-list.h"
+
+linked_list linked_list_new()
+{
+
+}
+
+void linked_list_append(linked_list ll, int elem)
+{
+
+}
+
+int linked_list_insert(linked_list ll, int elem, long idx)
+{
+
+}
+
+int linked_list_set(linked_list ll, int elem, long idx)
+{
+
+}
+
+int linked_list_delete(linked_list ll, int *dest, long idx)
+{
+
+}
+
+int linked_list_get(linked_list ll, int *dest, long idx)
+{
+
+}
+
+int linked_list_find(linked_list ll, int elem, long start, int mode)
+{
+
+}
+
+int linked_list_sort(linked_list ll)
+{
+
+}
+
+void linked_list_reverse(linked_list ll)
+{
+
+}
+
+void linked_list_print(linked_list ll)
+{
+
+}
+
+void linked_list_free(linked_list ll)
+{
+
+}
